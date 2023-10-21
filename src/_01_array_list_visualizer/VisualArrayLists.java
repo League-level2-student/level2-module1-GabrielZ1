96590be1baf735d59ef3d2ed37b481501bfe98af
league <league@imac-16.attlocal.net>
2023-10-21 11:31:42 -0700

    IntroToArrayLists, VisualArrayLists, and GuestBook

--- src/_01_array_list_visualizer/VisualArrayLists.java
+++ src/_01_array_list_visualizer/VisualArrayLists.java
@@ -11,29 +11,56 @@ import _01_array_list_visualizer.data.ArrayList;
 public class VisualArrayLists implements ActionListener {
 	Timer timer;
 	JFrame frame = new JFrame();
-	
+
 	ArrayList<String> list = new ArrayList<String>();
 
 	public void addThingsToList() {
 		// 1. Add a bunch of items to our list (4 or 5 should be enough!)
-		
+
+		list.add("eggs");
+		list.add("milk");
+		list.add("cheese");
+		list.add("cream");
+		list.add("chicken");
+
 		// 2. Set one item in our list to be something different
-		
+
+		list.set(4, "apple");
+
 		// 3. Remove something from our list
-		
+
+		list.remove(3);
+
 		// 4. Print the size of our list
-		
+
+		System.out.println(list.size());
+
 		// 5. Check if our list contains a specific thing of your choice
 		//	  and print whether it is in the list or not.
-		
+
+		if(list.contains("cheese")) {
+			System.out.println("cheese");
+		}
+		else {
+			System.out.println("no cheese");
+		}
+
 		// 6. Get a specific item from our list and print it out
-		
+
+		System.out.println(list.get(0));
+
 		// 7. Clear our list of all items
-		
+
+		list.clear();
+
 		// 8. Print the size of our list again
+
+		System.out.println(list.size());
+
 	}
-	
-	
+
+
+
 	/*
 	 * ------------DONT CHANGE ANYTHING BELOW THIS LINE-----------------------------------------
 	 */
--- src/_02_array_list_guestbook/GuestBook.java
+++ src/_02_array_list_guestbook/GuestBook.java
@@ -1,12 +1,83 @@
 package _02_array_list_guestbook;
 
-public class GuestBook {
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
-	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
-	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
-	// all the names added to the list. Format the list as follows:
-	// Guest #1: Bob Banders
-	// Guest #2: Sandy Summers
-	// Guest #3: Greg Ganders
-	// Guest #4: Donny Doners
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener {
+	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names".
+
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton addNameButton = new JButton();
+	JButton viewNamesButton = new JButton();
+
+
+	ArrayList<String> names = new ArrayList<String>();
+
+	public static void main(String[] args) {
+		GuestBook guestBook = new GuestBook();
+		guestBook.run();
+
+	}
+
+	public void run() {
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		addNameButton.addActionListener(this);
+		viewNamesButton.addActionListener(this);
+
+
+		frame.setTitle("Guest Book");
+
+		addNameButton.setText("Add Name");
+		viewNamesButton.setText("View Names");
+
+		panel.add(addNameButton);
+		panel.add(viewNamesButton);
+
+		frame.add(panel);
+
+		frame.pack();
+
+		frame.setVisible(true);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		JButton buttonClicked = (JButton)e.getSource();
+
+		// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
+		// that name to an ArrayList.
+
+		if(addNameButton == buttonClicked) {
+			names.add(JOptionPane.showInputDialog("Enter a name"));
+		}
+
+		// When the "View Names" button is clicked, display a message dialog that displays all the names added to the list.
+		// Format the list as follows:
+		// Guest #1: Bob Banders
+		// Guest #2: Sandy Summers
+		// Guest #3: Greg Ganders
+		// Guest #4: Donny Doners
+
+		else if(viewNamesButton == buttonClicked) {
+			String finalPrintMessage = "";
+			for(int i = 0; i < names.size(); i++) {
+				finalPrintMessage = finalPrintMessage + "Guest #" + (i+1) + ": " + names.get(i) + "\n";
+			}
+
+			JOptionPane.showMessageDialog(null, finalPrintMessage);
+
+		}
+
+	}
+
 }
